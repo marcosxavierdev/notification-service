bb8cd572ae805c4645c27b4e22ef529fdf9775bd
marcosxavierdev <sis.marcosxavier@gmail.com>
2024-10-24 15:42:13 -0300

    add endpoint health e ajuste pom

---
+++ src/main/java/com/consulting/notification_service/controller/NotificationService.java
@@ -0,0 +1,18 @@
+package com.consulting.notification_service.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/notification-service")
+public class NotificationService {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> getHealth() {
+        return ResponseEntity.ok("notification-service UP!");
+    }
+}
